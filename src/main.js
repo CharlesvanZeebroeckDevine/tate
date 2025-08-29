ae4d23bcf0b6f3e2b5847fff37feb26754b9491c
CharlesvanZeebroeckDevine <charles.van.zeebroeck@student.howest.be>
2025-08-29 11:28:49 +0200

    gsap

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline();
+
+tl.from(".showreel", {
+    opacity: 0,
+    duration: 1,
+    ease: "power2.inOut",
+});
